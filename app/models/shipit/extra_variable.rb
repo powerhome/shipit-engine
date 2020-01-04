bed07fc48b67e00924a0fe0e72e0026794ce6a12
Vinicius Gama <vinicius.sgama@gmail.com>
2020-01-03 21:13:57 -0300

    Uniqueness should be not be case sensitive

--- app/models/shipit/extra_variable.rb
+++ app/models/shipit/extra_variable.rb
@@ -3,6 +3,6 @@ module Shipit
     belongs_to :stack
     validates :key, :value, presence: true
     validates :key, uniqueness: { scope: :stack_id,
-      message: "key can only be defined once per stack" }
+      message: "key can only be defined once per stack", case_sensitive: false }
   end
 end
