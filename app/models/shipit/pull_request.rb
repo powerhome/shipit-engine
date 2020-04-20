12726e17fbab26223f4b068665f5ba278dfb4928
Vinicius Gama <vinicius.sgama@gmail.com>
2020-04-20 15:04:15 -0300

    Only add assignee if user is known

--- app/models/shipit/pull_request.rb
+++ app/models/shipit/pull_request.rb
@@ -270,7 +270,11 @@ module Shipit
     end
 
     def find_and_assign_users(pr_assignees)
-      pr_assignees.map { |assignee| User.find_by(login: assignee.login) || Shipit::AnonymousUser.new }
+      pr_assignees.map do |assignee|
+        assignee_user = User.find_by(login: assignee.login)
+        next unless assignee_user
+        assignee_user
+      end
     end
 
     def merge_message
