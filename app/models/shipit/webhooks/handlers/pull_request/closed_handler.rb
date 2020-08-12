7d589733b55445f729fdd16d61d5d3241c1e4c0a
Aaron Kuehler <aaron.kuehler@gmail.com>
2020-08-12 10:51:16 -0400

    Move PullRequestClosedHandler to shipit-engine

---
+++ app/models/shipit/webhooks/handlers/pull_request/closed_handler.rb
@@ -0,0 +1,38 @@
+# frozen_string_literal: true
+
+module Shipit
+  module Webhooks
+    module Handlers
+      module PullRequest
+        class ClosedHandler < Shipit::Webhooks::Handlers::Handler
+              params do
+                requires :action
+                requires :number
+                requires :repository
+                requires :sender
+              end
+
+          def process
+            return unless respond_to_pull_request_closed?
+
+            review_stack.archive!
+          end
+
+          private
+
+          def repository
+            @repository ||= Shipit::Repository.from_github_repo_name(params.repository["full_name"]) || Shipit::NullRepository.new
+          end
+
+          def review_stack
+            @review_stack ||= Shipit::Webhooks::Handlers::PullRequest::ReviewStackAdapter.new(params, scope: repository.review_stacks)
+          end
+
+          def respond_to_pull_request_closed?
+            params.action == "closed"
+          end
+        end
+      end
+    end
+  end
+end
