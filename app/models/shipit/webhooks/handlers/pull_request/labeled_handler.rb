1cf034fe854508987776d139d59f77a287414958
Aaron Kuehler <aaron.kuehler@gmail.com>
2020-08-17 15:04:23 -0400

    Move PullRequestLabeledHandler to shipit-engine

--- app/models/shipit/webhooks/handlers/pull_request/labeled_handler.rb
+++ app/models/shipit/webhooks/handlers/pull_request/labeled_handler.rb
@@ -4,7 +4,7 @@ module Shipit
   module Webhooks
     module Handlers
       module PullRequest
-        class ReopenedHandler < Shipit::Webhooks::Handlers::Handler
+        class LabeledHandler < Shipit::Webhooks::Handlers::Handler
           params do
             requires :action, String
             requires :number, Integer
@@ -35,34 +35,53 @@ module Shipit
           end
 
           def process
-            return unless respond_to_pull_request_reopened?
+            return unless respond_to_label_change?
 
-            stack.unarchive!
+            handle
           end
 
           private
 
-          def repository
-            @repository ||= Shipit::Repository.from_github_repo_name(params.repository.full_name) || Shipit::NullRepository.new
+          def handle
+            if archive?
+              stack.archive!
+            elsif unarchive?
+              stack.unarchive!
+            end
+
+            stack
           end
 
           def stack
             @stack ||= Shipit::Webhooks::Handlers::PullRequest::ReviewStackAdapter.new(params, scope: repository.review_stacks)
           end
 
+          def repository
+            @repository ||= Shipit::Repository.from_github_repo_name(params.repository.full_name) || Shipit::NullRepository.new
+          end
+
           def pull_request
             params.pull_request
           end
 
-          def respond_to_pull_request_reopened?
-            params.action == "reopened" &&
-              unarchive?
+          def pull_request_state
+            pull_request.state
+          end
+
+          def respond_to_label_change?
+            params.action == "labeled" &&
+              pull_request_state == "open" &&
+              repository.review_stacks_enabled &&
+              (archive? || unarchive?)
+          end
+
+          def archive?
+            (repository.provisioning_behavior_allow_with_label? && !pull_request_has_provisioning_label?) ||
+              (repository.provisioning_behavior_prevent_with_label? && pull_request_has_provisioning_label?)
           end
 
           def unarchive?
-            repository.review_stacks_enabled &&
-              repository.provisioning_behavior_allow_all? ||
-              (repository.provisioning_behavior_allow_with_label? && pull_request_has_provisioning_label?) ||
+            (repository.provisioning_behavior_allow_with_label? && pull_request_has_provisioning_label?) ||
               (repository.provisioning_behavior_prevent_with_label? && !pull_request_has_provisioning_label?)
           end
 
@@ -71,7 +90,7 @@ module Shipit
           end
 
           def pull_request_label_names
-            Array.new(pull_request["labels"]).map { |label| label["name"] }
+            Array.new(pull_request.labels).map { |label| label.name }
           end
         end
       end
