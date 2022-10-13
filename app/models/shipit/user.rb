dd538939d35f0a56d0729b58c2c2ef3d672551cb
Dang Huynh <dhhuynh2@gmail.com>
2022-10-13 13:39:20 -0400

    Logging changes

--- app/models/shipit/user.rb
+++ app/models/shipit/user.rb
@@ -31,7 +31,7 @@ module Shipit
     end
 
     def self.find_or_create_author_from_github_commit(github_commit)
-      pp("Hit")
+      Rails.logger.info("Hit")
       if (match_info = github_commit.commit.message.match(/^#{MergeRequest::MERGE_REQUEST_FIELD}: ([\w\-\.]+)$/))
         begin
           return find_or_create_by_login!(match_info[1])
@@ -45,6 +45,7 @@ module Shipit
       # We can either not use commit.author, and let it default to anonymous
       # Or we can use additional fields to try and match an author
       pp(github_commit)
+      Rails.logger.info("Github commit #{github_commit}")
       find_or_create_from_github(github_commit.author.presence || github_commit.commit.author.presence)
     end
 
